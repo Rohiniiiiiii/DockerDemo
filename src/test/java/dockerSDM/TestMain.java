2a62974b02ad4fe79e2071712a565c14286e32db
Group013 <Group013@infoway.com>
2023-01-25 10:39:09 +0530

    Added java  demo application files

---
+++ src/test/java/dockerSDM/TestMain.java
@@ -0,0 +1,14 @@
+package dockerSDM;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestMain {
+
+	@Test
+	public void testInputIsEven(){
+	 assertTrue(DockerSDM.checkIfInputIsAnEvenNumber(22)); // 
+	
+	 }
+}
